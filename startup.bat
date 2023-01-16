558b2c22668aa6b2d0884268455e268610d6c7bc
Welington Marquezini Valverde da Silva <w.valverde.da.silva@avanade.com>
2023-01-16 10:43:01 -0300

    ajuste no npm

--- startup.bat
+++ startup.bat
@@ -2,9 +2,9 @@ echo Iniciando Projeto BackEnd Api......
 start dotnet run --project .\src\GestaoEducacional.Api\GestaoEducacional.Api.csproj
 echo Iniciando Projeto BackEnd Api Pronto!!!
 echo Instalando pacotes npm
-start cd src\GestaoEducacional.View\ClientApp
+call cd src\GestaoEducacional.View\ClientApp
 start npm i
-start cd .\
+call cd .\
 echo Iniciando Projeto Front End Angular .....
 start dotnet run --project .\src\GestaoEducacional.View\GestaoEducacional.View.csproj
 echo Iniciando Projeto Front End Angular Pronto

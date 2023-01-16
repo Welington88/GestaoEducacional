871aba79659ce2280b2eaeaa31822d0be1a77c81
Welington Marquezini Valverde da Silva <w.valverde.da.silva@avanade.com>
2023-01-16 11:22:11 -0300

    ajustar sequencia do npm

--- startup.bat
+++ startup.bat
@@ -2,13 +2,14 @@ echo Iniciando Projeto BackEnd Api......
 start dotnet run --project .\src\GestaoEducacional.Api\GestaoEducacional.Api.csproj
 echo Iniciando Projeto BackEnd Api Pronto!!!
 echo Instalando pacotes npm
-call cd src\GestaoEducacional.View\ClientApp
-start npm i
-call cd .\
 echo Iniciando Projeto Front End Angular .....
 start dotnet run --project .\src\GestaoEducacional.View\GestaoEducacional.View.csproj
 echo Iniciando Projeto Front End Angular Pronto
+cd src\GestaoEducacional.View\ClientApp
+start npm i
+cd .\
+TIMEOUT 5
 start http://localhost:5068/swagger/index.html
-TIMEOUT 10
+TIMEOUT 5
 start http://localhost:5057/
 pause
